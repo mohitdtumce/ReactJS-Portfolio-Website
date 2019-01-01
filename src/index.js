71efb5fe99e3dc2f5b4804e51fa33204ccb810f8
SharmaMohit <mohit.sharma@tatahealth.com>
2019-01-02 00:40:22 +0530

    Removing code for forced https-ssl

--- src/index.js
+++ src/index.js
@@ -15,13 +15,6 @@ import { Provider } from "react-redux";
 
 import store from "./store";
 
-import express from 'express';
-import secure from 'ssl-express-www';
- 
-const app = express();
- 
-app.use(secure);
-
 ReactDOM.render(
     <Provider store={store}>
         <App />
